8db67f79cd066007dc23c5f6937798b7d1c51f79
Nicole Santamaria and Sara Schultz <nicole@>
2017-04-18 14:33:21 -0700

    pass first two specs and return 1 for all corresponding letters.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,8 +1,12 @@
 class String
   define_method(:scrabble) do
-    letters = Hash.new()
-    letters.store(1, 'A')
-    invert_hash = letters.invert()
-    invert_hash.fetch('A')
+    letters_equal_one = Array.new()
+    letters_equal_one.push('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')
+    letter_values = Array.new()
+    letter_values.push(1)
+
+    if letters_equal_one.include?(self)
+      letter_values.at(0)
+    end
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,7 @@ describe('String#scrabble') do
   it('returns a scrabble score for a letter') do
     expect('A'.scrabble()).to(eq(1))
   end
+  it('returns a scrabble score for a letter from an array') do
+    expect('E'.scrabble()).to(eq(1))
+  end
 end
