5eb2e18f01878d1783ea373f85f1c4c926cd51a8
Nicole Santamaria and Sara Schultz <sara@>
2017-04-18 14:38:47 -0700

    pass test for third and fourth spec for letters valuing two

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,11 +2,15 @@ class String
   define_method(:scrabble) do
     letters_equal_one = Array.new()
     letters_equal_one.push('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')
+    letters_equal_two = Array.new()
+    letters_equal_two.push('D', 'G')
     letter_values = Array.new()
-    letter_values.push(1)
+    letter_values.push(1, 2)
 
     if letters_equal_one.include?(self)
       letter_values.at(0)
+    elsif letters_equal_two.include?(self)
+      letter_values.at(1)
     end
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,10 +2,16 @@ require('rspec')
 require('scrabble')
 
 describe('String#scrabble') do
-  it('returns a scrabble score for a letter') do
+  it('returns a scrabble score for a letter valuing 1') do
     expect('A'.scrabble()).to(eq(1))
   end
-  it('returns a scrabble score for a letter from an array') do
+  it('returns a scrabble score for a letter from an array valuing 1') do
     expect('E'.scrabble()).to(eq(1))
+  end
+  it('returns a scrabble score for a letter valuing 2') do
+    expect('D'.scrabble()).to(eq(2))
+  end
+  it('returns a scrabble score for a letter from an array valuing 2') do
+    expect('G'.scrabble()).to(eq(2))
   end
 end
