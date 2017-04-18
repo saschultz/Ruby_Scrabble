ac7df523aed61d87c15384857bbcf63b67e1e739
Nicole Santamaria and Sara Schultz <sara@>
2017-04-18 14:41:12 -0700

    pass the fifth and sixth spec.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -4,13 +4,17 @@ class String
     letters_equal_one.push('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T')
     letters_equal_two = Array.new()
     letters_equal_two.push('D', 'G')
+    letters_equal_three = Array.new()
+    letters_equal_three.push('B', 'C', 'M', 'P')
     letter_values = Array.new()
-    letter_values.push(1, 2)
+    letter_values.push(1, 2, 3)
 
     if letters_equal_one.include?(self)
       letter_values.at(0)
     elsif letters_equal_two.include?(self)
       letter_values.at(1)
+    elsif letters_equal_three.include?(self)
+      letter_values.at(2)
     end
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -14,4 +14,10 @@ describe('String#scrabble') do
   it('returns a scrabble score for a letter from an array valuing 2') do
     expect('G'.scrabble()).to(eq(2))
   end
+  it('returns a scrabble score for a letter valuing 3') do
+    expect('M'.scrabble()).to(eq(3))
+  end
+  it('returns a scrabble score for a letter from an array valuing 3') do
+    expect('P'.scrabble()).to(eq(3))
+  end
 end
